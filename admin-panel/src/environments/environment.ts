6bfe675bf8f3890a656542d821dbd6c2add40635
Muskan Sirse <126276455+muskan-3210@users.noreply.github.com>
2024-12-05 15:22:25 +0530

    Update environment.ts

--- admin-panel/src/environments/environment.ts
+++ admin-panel/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
    production: false,
-   baseUrl: "http://localhost:5000/yogesh/api/v1",
-   imageUrl: "http://localhost:5000/"
+   baseUrl: "http://localhost:4000/yogesh/api/v1",
+   imageUrl: "http://localhost:4000/"
 
 }
