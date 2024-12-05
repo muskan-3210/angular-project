aa342d80a71eaf755dfb75cc08c8676bd60a3c85
muskan-3210 <muskan.sirseit2020@gmail.com>
2024-12-05 15:16:22 +0530

    Admin Changes

--- admin-panel/src/environments/environment.prod.ts
+++ admin-panel/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    API_URL: 'http://localhost:3000' 
+    API_URL: 'http://localhost:4000' 
     
 }
--- admin-panel/src/environments/environment.ts
+++ admin-panel/src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
     production: false,
-   // apiUrl11: "https://mmfinfotech.website/awsMarketing/public/api",
-   baseUrl: "http://localhost:5000/yogesh/api/v1",
-   imageUrl: "http://localhost:5000/"
+   baseUrl: "http://localhost:4000/yogesh/api/v1",
+   imageUrl: "http://localhost:4000/"
 
 }
